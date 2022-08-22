35d3269b7735935dca52f97a4601d5ab00ee22ae
danielwish <daniondev@gmail.com>
2022-08-22 04:06:19 -0400

    Script para BD
    Se agrega la creación de la Base

--- database/database.sql
+++ database/database.sql
@@ -1,3 +1,18 @@
+-- Database: postdb
+-- DROP DATABASE IF EXISTS postdb;
+CREATE DATABASE postdb
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Spain.1252'
+    LC_CTYPE = 'Spanish_Spain.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+COMMENT ON DATABASE postdb
+    IS 'Para app de posts en prueba de entrevista con TCIT';
+
 CREATE TABLE "post" (
   "id" SERIAL PRIMARY KEY NOT NULL,
   "nombre" varchar(100) NOT NULL,
